37aefa51d606ce21c2f48f5ebfe1d720be839263
Elliott Slaughter <slaughter@cs.stanford.edu>
2015-11-25 11:09:52 -0800

    Initial import.

---
+++ jupyterhub_config.py
@@ -0,0 +1,3 @@
+c.Spawner.notebook_dir = '~/notebooks'
+#c.Spawner.args = ['--NotebookApp.default_url=/notebooks/regent.ipynb']
+c.JupyterHub.extra_log_file = '/srv/jupyterhub/jupyterhub.log'
---
+++ kernels/regent/regentkernel.py
@@ -0,0 +1,100 @@
+# Copyright 2015 Stanford University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import os
+import time
+import stat
+from subprocess import Popen, PIPE
+from ipykernel.kernelbase import Kernel
+from datetime import datetime
+
+class RegentKernel(Kernel):
+    implementation = 'Regent'
+    implementation_version = '1.0'
+    language = 'regent'
+    language_version = '0.1'
+    language_info = { 'mimetype': 'text/x-regent', 'name': 'regent',
+            'pygments_lexer': 'lua', 'file_extension': 'rg' }
+    banner = "IPython kernel for Regent"
+
+    def do_execute(self, code, silent, store_history=True,
+            user_expressions=None, allow_stdin=False):
+        if not silent:
+            dir = datetime.now().strftime("kernellaunch-%Y-%m-%d-%M-%S.%f")
+            tmp_dir = os.path.join("/srv/jupyterhub/launches", dir)
+            os.mkdir(tmp_dir)
+            os.chmod(tmp_dir, stat.S_IRWXO + stat.S_IRWXG + stat.S_IRWXU)
+
+            regent_file = "test.rg"
+            torque_file = "run.sh"
+
+            regent_file_path = os.path.join(tmp_dir, regent_file)
+            torque_file_path = os.path.join(tmp_dir, torque_file)
+
+            with open(regent_file_path, "w") as file:
+                file.write(code)
+
+            num_nodes = 1
+            prof_file = "legion_prof_%.log"
+            prof_file_path = os.path.join(tmp_dir, prof_file)
+            regent_interpreter_path = os.path.join("/srv/jupyterhub",
+                    "legion/language/regent.py")
+
+            with open(torque_file_path, "w") as file:
+                file.write("#!/bin/bash -l\n")
+                file.write("#PBS -l nodes=%d\n" % num_nodes)
+                file.write("%s %s -hl:prof %d -level legion_prof=2 -logfile %s\n" % \
+                        (regent_interpreter_path,
+                            regent_file_path,
+                            num_nodes,
+                            prof_file_path))
+
+            stdout_file_path = os.path.join(tmp_dir, "stdout")
+            stderr_file_path = os.path.join(tmp_dir, "stderr")
+            # submit the job to torque
+            job_process = Popen(["qsub", torque_file_path,
+                "-o", stdout_file_path, "-e", stderr_file_path],
+                stdout=PIPE, stderr=PIPE)
+            job_id = job_process.stdout.read()
+
+            # wait until the job finishes
+            delay = 0.0001
+            check_result = ""
+            while check_result == "":
+                check_process = Popen(["qstat", "-f", job_id,
+                    "|", "grep", "\"job_state = C\""], stdout=PIPE, stderr=PIPE)
+                check_result = check_process.stdout.read().strip()
+                if not os.path.isfile(stdout_file_path) or \
+                        not os.path.isfile(stderr_file_path):
+                    check_result = ""
+                time.sleep(delay)
+                delay = delay * 2
+
+            with open(stdout_file_path, "r") as file:
+                output = file.read()
+
+            stream_content = {'name': 'stdout', 'text': output}
+            self.send_response(self.iopub_socket, 'stream', stream_content)
+
+        return {'status': 'ok',
+                # The base class increments the execution count
+                'execution_count': self.execution_count,
+                'payload': [],
+                'user_expressions': {},
+               }
+
+if __name__ == '__main__':
+    from ipykernel.kernelapp import IPKernelApp
+    IPKernelApp.launch_instance(kernel_class=RegentKernel)
