a8b9f41506af010d4691f7f94bfc1199526afa1c
Elliott Slaughter <slaughter@cs.stanford.edu>
2015-12-14 19:39:04 -0800

    Remove some cruft.

--- start-notebook.sh
+++ start-notebook.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-#exec su $NB_USER -c "env PATH=$PATH jupyter notebook $*"
-#exec su $NB_USER -c "env PATH=\"$PATH\" jupyter notebook $*"
-echo exec su $NB_USER -c "jupyter notebook $*"
 exec su $NB_USER -c "jupyter notebook $*"
